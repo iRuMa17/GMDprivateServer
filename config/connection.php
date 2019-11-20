05ba237d3fa33e88eae315a85a7f4f7ff6a90d2c
iRuMa <40019019+iRuMa17@users.noreply.github.com>
2019-11-20 17:11:01 +0900

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "geometrydash";
+$servername = "server ip"; //if your hosting application doesn't have unique ip, it will be "127.0.0.1" or "localhost" 
+$username = "your username";
+$password = "your pw";
+$dbname = "your db name";
 ?>
